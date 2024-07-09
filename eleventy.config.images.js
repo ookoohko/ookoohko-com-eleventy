4cfa296dc67b687f0c6975464f8f6f474cd6ea7a
Toni Laakso <toni@laaxo.com>
2024-07-09 15:18:05 +0300

    Replicate the images from the static site

--- eleventy.config.images.js
+++ eleventy.config.images.js
@@ -24,7 +24,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addAsyncShortcode("image", async function imageShortcode(src, alt, widths, sizes) {
 		// Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
 		// Warning: Avif can be resource-intensive so take care!
-		let formats = ["avif", "webp", "auto"];
+		let formats = ["auto"];
 		let input;
 		if(isFullUrl(src)) {
 			input = src;
@@ -35,7 +35,8 @@ module.exports = function(eleventyConfig) {
 		let metadata = await eleventyImage(input, {
 			widths: widths || ["auto"],
 			formats,
-			outputDir: path.join(eleventyConfig.dir.output, "img"), // Advanced usage note: `eleventyConfig.dir` works here because we’re using addPlugin.
+			outputDir: path.join(eleventyConfig.dir.output, "./images/"), // Advanced usage note: `eleventyConfig.dir` works here because we’re using addPlugin.
+			urlPath: "/images/",
 		});
 
 		// TODO loading=eager and fetchpriority=high
